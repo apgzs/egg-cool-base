82a89ee9d6e8ca435cd52f0e376f443b1e299942
啊平 <951984189@qq.com>
2019-11-06 10:56:59 +0800

    初始化提交

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    BaseController: require('./lib/base/controller'),
+    BaseEntity: require('./lib/base/entity'),
+    BaseService: require('/lib/base/service'),
+    router: require('./lib/router'),
+    cache: require('./lib/cache')
+};
